827b3d31ca50e4b7d95b635bfb7c8306a93ae7f7
narsipuraarnavi-ops <narsipuraarnavi@gmail.com>
2026-09-12 08:24:00 +0000

    Week 6: Added signal handling

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ src/main.c\
 src/input.c\
 src/parser.c\
 src/process.c\
-src/builtin.c
+src/builtin.c\
+src/signals.c
 
 TARGET=bin/shellforge
 
--- src/main.c
+++ src/main.c
@@ -7,11 +7,14 @@
 #include "../include/parser.h"
 #include "../include/process.h"
 #include "../include/builtin.h"
+#include "../include/signals.h"
 
 int main()
 {
     char *line;
     char **tokens;
+
+    initialize_signals();
 
     printf("=====================================\n");
     printf("ShellForge Version 3.0\n");
